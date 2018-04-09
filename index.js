2177b3a7a0cda8a370f7c86bb508fb3ddab8c817
Susanna Kosonen <kosonen.susanna@gmail.com>
2018-04-09 15:55:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  
+}
+
+function dec(n) {
+  
+}
+
+function makeInt(n) {
+  
+}
+
+function preserveDecimal(n) {
+  
+}
